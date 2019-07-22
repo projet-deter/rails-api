e2fc146d8bfaa89a97c89a85b14d9abf8d39db43
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-07-22 23:34:04 +0200

    feat/unit-test - Add comment unit-test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase  
-  test "the truth" do
-    assert true
+  # NAME
+  test "Should not save comment without body" do
+    comment = Comment.new
+    assert_not comment.save, "Saved the comment without a body"
+  end
+
+  test "Should not save comment with an empty body" do
+    comment = Comment.new(body: '')
+    assert_not comment.save, "Saved the comment with an empty body"
   end
 end

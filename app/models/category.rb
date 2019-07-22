c70ebd9be3f080fd230352dd7a4d6c9e8d682dcd
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-07-22 20:18:27 +0200

    feat/unit-test - Add category unit-test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :articles, dependent: :destroy
+
+  validates :name, presence: true, allow_blank: false
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not save category without name" do
+    category = Category.new
+    assert_not category.save, "Saved the category without a name"
+  end
+
+  test "Should not save category with empty name" do
+    category = Category.new(name: '')
+    assert_not category.save, "Saved the category with a empty name"
+  end
 end

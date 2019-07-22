6ea5943dd5194b5b5c9280389b46c0f4489f03f5
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-07-22 21:30:34 +0200

    feat/unit-test - Update category unit-test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
+  # NAME
   test "Should not save category without name" do
     category = Category.new
     assert_not category.save, "Saved the category without a name"
   end
 
-  test "Should not save category with empty name" do
+  test "Should not save category with an empty name" do
     category = Category.new(name: '')
-    assert_not category.save, "Saved the category with a empty name"
+    assert_not category.save, "Saved the category with an empty name"
   end
 end

0b982df2cd778b4558df6777e7cac3fcaf4bb39a
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-07-23 13:50:12 +0200

    feat/unit-test - Add an email regex for the user unit-test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   #validations
-  validates :email, presence: true, allow_blank: false, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, allow_blank: false, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: "only allows valid emails" }
   validates :password_digest, presence: true, allow_blank: false
   validates :name, presence: true, allow_blank: false
 
